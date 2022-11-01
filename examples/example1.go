811c08d7bc718ebf5144c912846ab1613779b804
Orkun Karaduman <orkunkaraduman@gmail.com>
2022-11-01 11:26:02 +0300

    updated example1

--- examples/example1.go
+++ examples/example1.go
@@ -20,9 +20,15 @@ func main() {
 		StrFlag     string  `name:"str" default:"abc" usage:"str usage"`
 		CustomFlag  float64 `name:"cust" usage:"custom flag usage"`
 		IgnoredFlag int64   `name:"-"`
+		FuncFlag    func(string) error
+		DefFlag     int `default:"35"`
+	}
+	st.FuncFlag = func(value string) error {
+		fmt.Println(value)
+		return nil
 	}
 	flagbind.Bind(fs, &st)
-	args := []string{"-bool-flag", "-bool-flag2", "true", "-int-flag", "10", "-str", "def", "-cust", "10.6"}
+	args := []string{"-bool-flag", "-bool-flag2", "true", "-int-flag", "10", "-str", "def", "-cust", "10.6", "-func-flag", "aaa"}
 	_ = fs.Parse(args)
 	fmt.Printf("%+v\n", st)
 }
